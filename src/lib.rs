882f330a9cebbe8e6393350699c620ff751108ae
thewhoaia <12012108+thewhoaia@users.noreply.github.com>
2025-02-12 23:39:04 +0100

    Add rounded rect

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 #![allow(clippy::missing_safety_doc)]
 
 use std::ffi::{c_char, CStr};
-use tiny_skia::{BlendMode, Color, FillRule, Paint, Path, PathBuilder, Point, Rect, Transform};
+use tiny_skia::{
+    BlendMode, Color, FillRule, Paint, Path, PathBuilder, Point, Rect, Stroke, Transform,
+};
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -15,14 +17,14 @@ pub struct ts_point {
 #[derive(Copy, Clone)]
 pub enum ts_blend_mode {
     SourceOver,
-    SourceCopy
+    SourceCopy,
 }
 
 impl From<ts_blend_mode> for BlendMode {
     fn from(value: ts_blend_mode) -> Self {
         match value {
             ts_blend_mode::SourceOver => BlendMode::SourceOver,
-            ts_blend_mode::SourceCopy => BlendMode::Source
+            ts_blend_mode::SourceCopy => BlendMode::Source,
         }
     }
 }
@@ -91,6 +93,21 @@ impl From<ts_color> for Color {
     }
 }
 
+#[repr(C)]
+pub enum ts_fill_rule {
+    Winding,
+    EvenOdd
+}
+
+impl From<ts_fill_rule> for FillRule {
+    fn from(value: ts_fill_rule) -> Self {
+        match value {
+            ts_fill_rule::Winding => FillRule::Winding,
+            ts_fill_rule::EvenOdd => FillRule::EvenOdd
+        }
+    }
+}
+
 pub struct ts_pixmap(tiny_skia::Pixmap);
 
 #[repr(C)]
@@ -132,6 +149,18 @@ pub extern "C" fn ts_transform_scale(sx: f32, sy: f32) -> ts_transform {
 }
 
 #[no_mangle]
+pub extern "C" fn ts_transform_translate(tx: f32, ty: f32) -> ts_transform {
+    ts_transform {
+        sx: 1.0,
+        kx: 0.0,
+        ky: 0.0,
+        sy: 1.0,
+        tx,
+        ty,
+    }
+}
+
+#[no_mangle]
 pub extern "C" fn ts_transform_rotate(angle: f32) -> ts_transform {
     Transform::from_rotate(angle).into()
 }
@@ -164,6 +193,16 @@ pub unsafe extern "C" fn ts_line_to(p: *mut ts_path_builder, x: f32, y: f32) {
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn ts_quad_to(p: *mut ts_path_builder, x0: f32, y0: f32, x1: f32, y1: f32) {
+    (*p).0.quad_to(x0, y0, x1, y1);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ts_cubic_to(p: *mut ts_path_builder, x0: f32, y0: f32, x1: f32, y1: f32, x2: f32, y2: f32) {
+    (*p).0.cubic_to(x0, y0, x1, y1, x2, y2);
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn ts_close(p: *mut ts_path_builder) {
     (*p).0.close();
 }
@@ -172,6 +211,33 @@ pub unsafe extern "C" fn ts_close(p: *mut ts_path_builder) {
 pub unsafe extern "C" fn ts_path_builder_finish(b: *mut ts_path_builder) -> *mut ts_path {
     let path_builder = Box::from_raw(b);
     Box::into_raw(Box::new(ts_path(path_builder.0.finish().unwrap())))
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ts_rounded_rect(rect: ts_rect, mut rx: f32, mut ry: f32) -> *mut ts_path {
+    let (x, y, width, height) = (rect.x0, rect.y0, rect.x1 - rect.x0, rect.y1- rect.y0);
+
+    rx = rx.min(width / 2.0);
+    ry = ry.min(height / 2.0);
+
+    let mut builder = PathBuilder::new();
+    builder.move_to(x + rx, y);
+
+    builder.line_to(x + width - rx, y);
+    builder.arc_to(rx, ry, 0.0, false, true, x + width, y + ry);
+
+    builder.line_to(x + width, y + height - ry);
+    builder.arc_to(rx, ry, 0.0, false, true, x + width - rx, y + height);
+
+    builder.line_to(x + rx, y + height);
+    builder.arc_to(rx, ry, 0.0, false, true, x, y + height - ry);
+
+    builder.line_to(x, y + ry);
+    builder.arc_to(rx, ry, 0.0, false, true, x + rx, y);
+
+    builder.close();
+
+    Box::into_raw(Box::new(ts_path(builder.finish().unwrap())))
 }
 
 #[no_mangle]
@@ -207,7 +273,8 @@ pub unsafe extern "C" fn ts_pixmap_fill_path(
     path: *const ts_path,
     transform: ts_transform,
     color: ts_color,
-    blend_mode: ts_blend_mode
+    fill_rule: ts_fill_rule,
+    blend_mode: ts_blend_mode,
 ) {
     let mut paint = Paint {
         blend_mode: blend_mode.into(),
@@ -218,9 +285,9 @@ pub unsafe extern "C" fn ts_pixmap_fill_path(
     (*pixmap).0.fill_path(
         &(*path).0,
         &paint,
-        FillRule::Winding,
+        fill_rule.into(),
         transform.into(),
-        None
+        None,
     );
 }
 
@@ -230,7 +297,7 @@ pub unsafe extern "C" fn ts_pixmap_fill_rect(
     rect: ts_rect,
     transform: ts_transform,
     color: ts_color,
-    blend_mode: ts_blend_mode
+    blend_mode: ts_blend_mode,
 ) {
     let mut paint = Paint {
         blend_mode: blend_mode.into(),
@@ -238,13 +305,9 @@ pub unsafe extern "C" fn ts_pixmap_fill_rect(
     };
     paint.set_color_rgba8(color.r, color.g, color.b, color.a);
 
-
-    (*pixmap).0.fill_rect(
-        rect.into(),
-        &paint,
-        transform.into(),
-        None
-    );
+    (*pixmap)
+        .0
+        .fill_rect(rect.into(), &paint, transform.into(), None);
 }
 
 pub struct ts_argb(Vec<u8>);
@@ -270,4 +333,126 @@ pub unsafe extern "C" fn ts_argb_data(data: *const ts_argb) -> *const u8 {
 #[no_mangle]
 pub unsafe extern "C" fn ts_argb_destroy(data: *mut ts_argb) {
     let _ = Box::from_raw(data);
+}
+
+#[repr(C)]
+pub struct ts_stroke {
+    width: f32
+}
+
+impl From<ts_stroke> for Stroke {
+    fn from(value: ts_stroke) -> Self {
+        Self {
+            width: value.width,
+            ..Default::default()
+        }
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ts_pixmap_stroke_path(
+    pixmap: *mut ts_pixmap,
+    path: *const ts_path,
+    transform: ts_transform,
+    color: ts_color,
+    stroke: ts_stroke,
+    blend_mode: ts_blend_mode,
+) {
+    let mut paint = Paint {
+        blend_mode: blend_mode.into(),
+        ..Default::default()
+    };
+    paint.set_color_rgba8(color.r, color.g, color.b, color.a);
+
+    (*pixmap).0.stroke_path(
+        &(*path).0,
+        &paint,
+        &stroke.into(),
+        transform.into(),
+        None,
+    );
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ts_pixmap_stroke_rect(
+    pixmap: *mut ts_pixmap,
+    rect: ts_rect,
+    transform: ts_transform,
+    color: ts_color,
+    stroke: ts_stroke,
+    blend_mode: ts_blend_mode,
+) {
+    let mut paint = Paint {
+        blend_mode: blend_mode.into(),
+        ..Default::default()
+    };
+    paint.set_color_rgba8(color.r, color.g, color.b, color.a);
+
+    (*pixmap)
+        .0
+        .stroke_path(
+            &PathBuilder::from_rect(rect.into()),
+            &paint,
+            &stroke.into(),
+            transform.into(),
+            None,
+        );
+}
+
+trait PathBuilderExt {
+    fn arc_to(
+        &mut self,
+        rx: f32,
+        ry: f32,
+        x_axis_rotation: f32,
+        large_arc: bool,
+        sweep: bool,
+        x: f32,
+        y: f32,
+    );
+}
+
+impl PathBuilderExt for PathBuilder {
+    fn arc_to(
+        &mut self,
+        rx: f32,
+        ry: f32,
+        x_axis_rotation: f32,
+        large_arc: bool,
+        sweep: bool,
+        x: f32,
+        y: f32,
+    ) {
+        let prev = match self.last_point() {
+            Some(v) => v,
+            None => return,
+        };
+
+        let svg_arc = kurbo::SvgArc {
+            from: kurbo::Point::new(prev.x as f64, prev.y as f64),
+            to: kurbo::Point::new(x as f64, y as f64),
+            radii: kurbo::Vec2::new(rx as f64, ry as f64),
+            x_rotation: (x_axis_rotation as f64).to_radians(),
+            large_arc,
+            sweep,
+        };
+
+        match kurbo::Arc::from_svg_arc(&svg_arc) {
+            Some(arc) => {
+                arc.to_cubic_beziers(0.1, |p1, p2, p| {
+                    self.cubic_to(
+                        p1.x as f32,
+                        p1.y as f32,
+                        p2.x as f32,
+                        p2.y as f32,
+                        p.x as f32,
+                        p.y as f32,
+                    );
+                });
+            }
+            None => {
+                self.line_to(x, y);
+            }
+        }
+    }
 }
